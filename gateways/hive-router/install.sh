8d710b11ed4ab2822827769688a337caf5f4a2f5
enisdenjo <badurinadenis@gmail.com>
2025-11-10 23:24:14 +0100

    latest router

--- gateways/hive-router/install.sh
+++ gateways/hive-router/install.sh
@@ -1,2 +1,2 @@
 # https://github.com/graphql-hive/router/releases
-curl -o- https://raw.githubusercontent.com/graphql-hive/router/main/install.sh | sh -s v0.0.5
+curl -o- https://raw.githubusercontent.com/graphql-hive/router/main/install.sh | sh -s v0.0.17
