f5ae7561f3b380516ab516109e7de432ee87de45
enisdenjo <badurinadenis@gmail.com>
2025-11-11 18:29:02 +0100

    fix run

--- gateways/hive-gateway-router-runtime/run.sh
+++ gateways/hive-gateway-router-runtime/run.sh
@@ -1,2 +1,3 @@
-npm start supergraph -- --cwd ./gateways/hive-gateway --test $1
+# cant use "npm start" because there's a package.json here
+node --import tsx ../../src/cli.ts supergraph --cwd . --test $1
 ./node_modules/.bin/hive-gateway supergraph supergraph.graphql --port 4000
