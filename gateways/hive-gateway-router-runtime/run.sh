2f67e555e526e0aeab23ebdf37035a42c39458f0
enisdenjo <badurinadenis@gmail.com>
2025-11-12 11:15:01 +0100

    router runtime npm start

--- gateways/hive-gateway-router-runtime/run.sh
+++ gateways/hive-gateway-router-runtime/run.sh
@@ -1,2 +1,2 @@
-npm start supergraph -- --cwd ./gateways/hive-gateway --test $1
+npm start supergraph -- --cwd ./gateways/hive-gateway-router-runtime --test $1
 npx hive-gateway supergraph supergraph.graphql --port 4000 --hive-router-runtime --fork 1
